d2a385bd88b60d652e37088dec0db47a9c88612d
jsanteler <juliansanteler@tsn.at>
2023-12-13 09:22:36 +0100

    Datensätze abfragen mit deleteStudentDemo();

--- src/main/java/org/example/Jdbcdemo.java
+++ src/main/java/org/example/Jdbcdemo.java
@@ -14,7 +14,36 @@ public class Jdbcdemo {
         updateStudentDemo(4, "Neuer Name", "neueemail@gmail.com");
         selectAllDemo();
         deleteStudentDemo(5);
+        selectAllDemo();
+        findAllByNameLike("er");
 
+    }
+
+    private static void findAllByNameLike(String pattern) {
+
+        System.out.println("Find all by Name Demo mit JDBC");
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "1234";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl,user,pwd))
+        {
+            System.out.println("Verbindung zur DB hergestellt!");
+
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `student` WHERE `student`.`name` LIKE ?");
+            preparedStatement.setString(1,"%" +pattern +"%");
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                System.out.println("Student aus der DB: [ID] " + id + " [NAME] " + name + " [EMAIL] " + email);
+            }
+        } catch (SQLException e){
+            System.out.println("Fehler beim Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
     }
 
 
